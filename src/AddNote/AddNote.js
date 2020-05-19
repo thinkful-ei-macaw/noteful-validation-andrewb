444b7b850b70ee6b52b58296ef324550c1fd0d0c
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-18 19:08:11 -0700

    add one more

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -26,7 +26,7 @@ class AddNote extends React.Component {
             name: e.target['note-name'].value,
             content: e.target['content-name'].value,
             folder_id: e.target.folderId.value,
-            modified: new Date().toLocaleDateString()
+            modified: new Date()
         }
         console.log(newNote)
         fetch(`${config.API_ENDPOINT}/notes`, {
